d2ab34042e9831a3f8c62afe925fdaae62989504
giapl <ledanggiap1704@gmail.com>
2024-01-10 21:40:06 +0700

    update_v1

--- start-project.sh
+++ start-project.sh
@@ -6,7 +6,7 @@ echo "build image"
 docker build -f Dockerfile -t app_noron .
 # down project
 echo "down project"
-docker-compose -f Docker-compose.yml down
+docker-compose -f Docker-compose.yml rm -f noron_app
 # start project
 echo "start project"
 docker-compose -f Docker-compose.yml up -d
